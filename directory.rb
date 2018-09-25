5c13b90d8beff80eac5ef16c2a1510a002ca5e40
Alfie Freeman <runticle@gmail.com>
2018-09-25 15:23:17 +0100

    added option to list by cohort or by name

--- directory.rb
+++ directory.rb
@@ -9,10 +9,14 @@ def input_students
   puts "Enter student cohort"
   input_cohort = gets.chomp
   input_cohort == "" ? cohort = "november" : cohort = input_cohort
+  puts "Enter students' height in metres"
+  height = gets.chomp.to_s
+  puts "Enter students' nationality"
+  nation = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add student hash to the array
-    students << {name: name, cohort: cohort.to_sym}
+    students << {name: name, cohort: cohort.to_sym, nation: nation, height: height}
     puts "Now we have #{students.count} student#{+ "s" if students.count > 1}"
     # get another name from the user
     puts "Please enter another student"
@@ -21,6 +25,10 @@ def input_students
     puts "Enter student cohort"
     input_cohort = gets.chomp
     input_cohort == "" ? cohort = "november" : cohort = input_cohort
+    puts "Enter students' height in metres"
+    height = gets.chomp.to_s
+    puts "Enter students' nationality"
+    nation = gets.chomp.capitalize
   end
   # returns the array of students
   students
@@ -30,7 +38,20 @@ def print_header
   puts "-------------".center(100)
 end
 def print(students)
-  #  create cohorts hash
+  puts "Enter 1 to print by cohort or 2 to print by name"
+  user = gets.chomp.to_i
+  if user == 1
+    print_header
+    print_by_cohort(students)
+  elsif user == 2
+    print_header
+    print_by_name(students)
+  else
+    puts "Invalid option"
+    exit
+  end
+end
+def print_by_cohort(students)
   cohorts_list = {}
   students.each { |i|
     name = i[:name]
@@ -41,7 +62,16 @@ def print(students)
       cohorts_list[cohort].push(name)
     end
   }
-    cohorts_list.each { |cohort, names| "#{puts (cohort.to_s + " Cohort").to_s.capitalize.center(100)} #{names.each { |name| puts name.center(100) }}" }
+    cohorts_list.each { |cohort, names| "#{puts (cohort.to_s + " Cohort").capitalize.center(100)} #{names.each { |name| puts name.center(100) }}" }
+end
+def print_by_name(students)
+  if students.count > 0
+    i = 0
+    until students.count == i do
+      puts "#{i + 1}. #{students[i][:name]} (Height: #{students[i][:height]}m, Nationality: #{students[i][:nation]}, #{students[i][:cohort]} cohort)".center(100) if students[i][:name].length < 12
+      i += 1
+    end
+  end
 end
 def print_footer(names)
   if names.count > 0
@@ -52,6 +82,5 @@ def print_footer(names)
 end
 # nothing happens until we call the methods
 students = input_students
-print_header
 print(students)
 print_footer(students)
