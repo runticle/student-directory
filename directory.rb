1dbb68c067e2a1e526920e7d1f4876a28bae671b
Alfie Freeman <runticle@gmail.com>
2018-09-25 15:04:07 +0100

    now lists students by cohort & removed height/nationality info

--- directory.rb
+++ directory.rb
@@ -9,26 +9,18 @@ def input_students
   puts "Enter student cohort"
   input_cohort = gets.chomp
   input_cohort == "" ? cohort = "november" : cohort = input_cohort
-  puts "Enter students' height in metres"
-  height = gets.chomp.to_s
-  puts "Enter students' nationality"
-  nation = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add student hash to the array
-    students << {name: name, cohort: cohort.to_sym, nation: nation, height: height}
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} student#{+ "s" if students.count > 1}"
     # get another name from the user
     puts "Please enter another student"
-    name = gets.delete("\n").capitalize
+    name = gets.chomp.capitalize
     # add some more infomation
     puts "Enter student cohort"
-    input_cohort = gets.delete("\n")
+    input_cohort = gets.chomp
     input_cohort == "" ? cohort = "november" : cohort = input_cohort
-    puts "Enter students' height in metres"
-    height = gets.delete("\n").to_s
-    puts "Enter students' nationality"
-    nation = gets.delete("\n").capitalize
   end
   # returns the array of students
   students
@@ -38,13 +30,18 @@ def print_header
   puts "-------------".center(100)
 end
 def print(students)
-  if students.count > 0
-    i = 0
-    until students.count == i do
-      puts "#{i + 1}. #{students[i][:name]} (Height: #{students[i][:height]}m, Nationality: #{students[i][:nation]}, #{students[i][:cohort]} cohort)".center(100) if students[i][:name].length < 12
-      i += 1
+  #  create cohorts hash
+  cohorts_list = {}
+  students.each { |i|
+    name = i[:name]
+    cohort = i[:cohort]
+    if cohorts_list[cohort] == nil
+      cohorts_list[cohort] = [name]
+    else
+      cohorts_list[cohort].push(name)
     end
-  end
+  }
+    cohorts_list.each { |cohort, names| "#{puts (cohort.to_s + " Cohort").to_s.capitalize.center(100)} #{names.each { |name| puts name.center(100) }}" }
 end
 def print_footer(names)
   if names.count > 0
