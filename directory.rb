2fb7cc072342f894d33d6148fb73458e90d61211
Alfie Freeman <runticle@gmail.com>
2018-09-25 11:59:27 +0100

    added height and nationality infomation

--- directory.rb
+++ directory.rb
@@ -1,17 +1,28 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return thrice once all info has been added"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp.capitalize
+  # add some more infomation
+  puts "Enter students' height in metres"
+  height = gets.chomp.to_s
+  puts "Enter students' nationality"
+  nation = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, nation: nation, height: height}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Please enter another student"
     name = gets.chomp.capitalize
+    # add some more infomation
+    puts "Enter students' height in metres"
+    height = gets.chomp.to_s
+    puts "Enter students' nationality"
+    nation = gets.chomp.capitalize
   end
   # returns the array of students
   students
@@ -23,7 +34,7 @@ end
 def print(students)
   i = 0
   until students.count == i do
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)" if students[i][:name].length < 12
+    puts "#{i + 1}. #{students[i][:name]} (Height: #{students[i][:height]}m, Nationality: #{students[i][:nation]}, #{students[i][:cohort]} cohort)" if students[i][:name].length < 12
     i += 1
   end
 
